bc9dab42501caf686c62f982a0aab59e6516d16a
Diego Rodríguez Guzmán <diegorem99@gmail.com>
2024-01-03 12:47:22 -0600

    Inicio D8

---
+++ D8/Ejercicio.py
@@ -0,0 +1,4 @@
+from mi_paquete import suma_y_resta
+
+print(suma_y_resta.suma(1, 2))
+print(suma_y_resta.resta(1, 2))
---
+++ D8/ModuloOcupado.py
@@ -0,0 +1,2 @@
+def saludar():
+    print("Hola, te estoy saludando desde la funcion saludar del modulo ocupado")
