36a6e31a17d3459b3a29411e8e4fda6ecc1c0047
Diego Rodríguez Guzmán <diegorem99@gmail.com>
2024-01-03 12:28:32 -0600

    Día 7 final

--- D7 OOP/Metodos_especiales.py
+++ D7 OOP/Metodos_especiales.py
@@ -21,3 +21,16 @@ print(mi_cd)
 print(len(mi_cd))
 del mi_cd
 
+
+class Libro():
+    def __init__(self, titulo, autor, cantidad_paginas):
+        self.titulo = titulo
+        self.autor = autor
+        self.cantidad_paginas = cantidad_paginas
+
+    def __str__(self):
+        return f'"{self.titulo}", de {self.autor}'
+
+
+it = Libro("It", "Stephen King", 1138)
+print(it)
---
+++ D7 OOP/Proyecto7.py
@@ -0,0 +1,59 @@
+class Persona:
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+
+class Empleado(Persona):
+    def __init__(self, nombre, apellido, numero_empleado, balance):
+        super().__init__(nombre, apellido)
+        self.numero_empleado = numero_empleado
+        self.balance = balance
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellido} tiene un balance de {self.balance}'
+
+    def depositar(self, cantidad):
+        self.balance += cantidad
+
+    def retirar(self, cantidad):
+        self.balance -= cantidad
+
+
+
+print("Bienvenido al sistema de empleados")
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+numero_empleado = input("Ingrese su numero de empleado: ")
+balance = float(input("Ingrese su balance: "))
+empleado = Empleado(nombre, apellido, numero_empleado, balance)
+
+# menu
+band = 1
+while band == 1:
+    print("\nMenu")
+    print("1. Depositar")
+    print("2. Retirar")
+    print("3. Consultar balance")
+    print("4. Salir")
+    # Un try en caso de que el usuario ingrese un string
+    try:
+        opcion = int(input('Ingrese una opción: '))
+    except:
+        opcion = 0
+    if opcion == 1:
+        cantidad = float(input("Ingrese la cantidad a depositar: "))
+        empleado.depositar(cantidad)
+    elif opcion == 2:
+        cantidad = float(input("Ingrese la cantidad a retirar: "))
+        if cantidad > empleado.balance:
+            print("No puede retirar más de lo que tiene")
+        else:
+            empleado.retirar(cantidad)
+    elif opcion == 3:
+        print(empleado)
+    elif opcion == 4:
+        band = 0
+    else:
+        print("Opción incorrecta")
+
+print("Gracias por usar el sistema")
